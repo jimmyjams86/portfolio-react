005ccdb37af27197dbe57eecb765e2cb88dd032f
jimmyjams86 <58612656+jimmyjams86@users.noreply.github.com>
2025-02-25 17:12:09 -0500

    Remove react-router and unnecessary routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Hero from './components/Hero'
-import InteractiveSphere from './components/InteractiveSphere' // Import the new component
 
 function App() {
   return (
-    <Router>
-      <div className="w-full">
-        {/* Routes for Pages */}
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/interactive-sphere" element={<InteractiveSphere />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="w-full">
+      <Hero />
+    </div>
   )
 }
 
